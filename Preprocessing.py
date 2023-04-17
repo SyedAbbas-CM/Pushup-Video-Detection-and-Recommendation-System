7dcf7f8b28f8af5aaca844523ce34206e28b27aa
Syed Abbas <76508415+SyedAbbas-CM@users.noreply.github.com>
2023-04-17 23:38:04 +0500

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,73 +1,57 @@
 import os
+os.environ['OPENCV_VIDEOIO_PRIORITY_MSMF'] = '1'
 import cv2
 import json
-import numpy as np
-from sklearn.model_selection import train_test_split
 import shutil
+from distutils import dir_util
 
 def preprocess_data(base_dir, output_dir):
-    video_dir = os.path.join(base_dir, 'dataset', 'pushups', 'pushups', 'data', 'Videos')
-    json_dir = os.path.join(base_dir, 'dataset', 'pushups', 'pushups', 'data', 'Json')
-    label_dir = os.path.join(base_dir, 'dataset', 'pushups', 'pushups', 'data', 'labels')
+    video_dir = os.path.join(base_dir, "dataset", "pushups", "pushups", "data", "videos")
+    json_dir = os.path.join(base_dir, "dataset", "pushups", "pushups", "data", "json")
+    label_dir = os.path.join(base_dir, "dataset", "pushups", "pushups", "data", "labels")
 
-    train_output = os.path.join(output_dir, 'train')
-    val_output = os.path.join(output_dir, 'val')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    os.makedirs(train_output, exist_ok=True)
-    os.makedirs(val_output, exist_ok=True)
+    for video_name in os.listdir(video_dir):
+        video_path = os.path.join(video_dir, video_name)
+        json_path = os.path.join(json_dir, os.path.splitext(video_name)[0] + ".json")
+        label_path = os.path.join(label_dir, os.path.splitext(video_name)[0] + "_img_labels")
 
-    video_files = [f for f in os.listdir(video_dir) if f.endswith('.mp4')]
-    json_files = [f for f in os.listdir(json_dir) if f.endswith('.json')]
-
-    train_videos, val_videos, _, _ = train_test_split(video_files, json_files, test_size=0.2, random_state=42)
-
-    for video_file in train_videos:
-        video_path = os.path.join(video_dir, video_file)
-        json_path = os.path.join(json_dir, video_file.replace('.mp4', '.json'))
-        label_path = os.path.join(label_dir, video_file.replace('.mp4', ''))
-
-        video_output_path = os.path.join(train_output, video_file)
-        json_output_path = os.path.join(train_output, video_file.replace('.mp4', '.json'))
-        label_output_path = os.path.join(train_output, video_file.replace('.mp4', ''))
-
-        cap = cv2.VideoCapture(video_path, cv2.CAP_FFMPEG)
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-
-        if fps == 0:
-            print(f"Error: Video file {video_file} has unsupported format or cannot be read.")
+        if not os.path.exists(video_path) or not os.path.exists(json_path):
+            print(f"Error: Video file {video_name} or JSON file {os.path.splitext(video_name)[0]}.json not found.")
             continue
+
+        video_output_dir = os.path.join(output_dir, "videos", os.path.splitext(video_name)[0])
+        json_output_path = os.path.join(output_dir, "json", os.path.splitext(video_name)[0] + ".json")
+        label_output_path = os.path.join(output_dir, "labels", os.path.splitext(video_name)[0])
+
+        if not os.path.exists(video_output_dir):
+            os.makedirs(video_output_dir)
+        if not os.path.exists(os.path.dirname(json_output_path)):
+            os.makedirs(os.path.dirname(json_output_path))
+        if not os.path.exists(label_output_path) and os.path.exists(label_path):
+            os.makedirs(label_output_path)
+
+        cap = cv2.VideoCapture(video_path)
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        for idx in range(frame_count):
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            if idx % fps == 0:
+                frame_output_path = os.path.join(video_output_dir, f"{idx:05d}.png")
+                cv2.imwrite(frame_output_path, frame)
 
         cap.release()
 
-        with open(json_path, 'r') as f:
-            keypoints = json.load(f)
-
-        resampled_keypoints = []
-        for idx, frame_keypoints in enumerate(keypoints):
-            if idx % fps == 0:
-                resampled_keypoints.append(frame_keypoints)
-
-        with open(json_output_path, 'w') as f:
-            json.dump(resampled_keypoints, f)
-
-        shutil.copy(video_path, video_output_path)
-        shutil.copytree(label_path, label_output_path)
-
-    for video_file in val_videos:
-        video_path = os.path.join(video_dir, video_file)
-        json_path = os.path.join(json_dir, video_file.replace('.mp4', '.json'))
-        label_path = os.path.join(label_dir, video_file.replace('.mp4', ''))
-
-        video_output_path = os.path.join(val_output, video_file)
-        json_output_path = os.path.join(val_output, video_file.replace('.mp4', '.json'))
-        label_output_path = os.path.join(val_output, video_file.replace('.mp4', ''))
-
-        shutil.copy(video_path, video_output_path)
         shutil.copy(json_path, json_output_path)
-        shutil.copytree(label_path, label_output_path)
+        if os.path.exists(label_path):
+            dir_util.copy_tree(label_path, label_output_path)
 
-
-
-base_dir = ".."  # Assuming the dataset is one level above the project folder
-output_dir = "output"  # Output folder will be created in the project folder
+base_dir = ".."
+output_dir = "preprocessed_data"
 preprocess_data(base_dir, output_dir)
