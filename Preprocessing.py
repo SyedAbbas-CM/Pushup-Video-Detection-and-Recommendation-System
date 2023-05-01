5d756498cd2f03fd71bfff7b6b732e8c13dc5279
Syed Abbas <76508415+SyedAbbas-CM@users.noreply.github.com>
2023-05-02 03:25:41 +0500

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,6 +4,8 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
+label_to_index = {"pushup": 0, "curl": 1, "fly": 2, "squat": 3, "ohp": 4, "non_exercise": 5}
+
 keypoint_names = [
     "nose",
     "left_eye",
@@ -27,8 +29,6 @@ dtype = []
 for name in keypoint_names:
     dtype.append((name, np.float32, (3,)))
 
-
-
 def normalize_keypoints(keypoints, width, height):
     normalized_keypoints = []
     for i in range(0, len(keypoints), 3):
@@ -40,29 +40,12 @@ def normalize_keypoints(keypoints, width, height):
     #print(normalized_keypoints)
     return normalized_keypoints
 
-# not used for now; it could be used to enchance model and do 3d pose estimation
-# This is specifically used to normalize the Z (DEPTH) values of the armature points; we will need the maximum Z value
-def find_max_z(folder_path):
-    max_z = 0
-    for file in os.listdir(folder_path):
-        if file.endswith(".json"):
-            file_path = os.path.join(folder_path, file)
-            with open(file_path) as json_file:
-                data = json.load(json_file)
-                for annotation in data['annotations']:
-                    armature_keypoints = annotation.get('armature_keypoints', {})
-                    for key, value in armature_keypoints.items():
-                        z = value['z']
-                        if z > max_z:
-                            max_z = z
-    return max_z
+def create_sequences(data, sequence_length, step_size):
+    sequences = np.stack([data[i:i + sequence_length] for i in range(0, data.shape[0] - sequence_length + 1, step_size)], axis=0)
+    return sequences
 
-# convert the dictionary into a list so armature keypoints and keypoints can be normalized together
-def armature_keypoints_to_list(armature_keypoints):
-    keypoints_dict = {}
-    for key, value in armature_keypoints.items():
-        keypoints_dict[key] = [value['x'], value['y'], value['v']]
-    return keypoints_dict
+sequence_length = 60  # Adjust this based on your requirements
+step_size = 10  # Adjust this based on your requirements
 
 
 def preprocess_data(folder_path, label):
@@ -79,61 +62,65 @@ def preprocess_data(folder_path, label):
         print(f"Error: No JSON files found in folder {folder_path}.")
         return
 
-    print(f"Processing {len(json_files)} JSON files in folder {folder_path}...")
-
     for file in json_files:
+        file_data = []  # Store the data for the current file
         file_path = os.path.join(folder_path, file)
-        print(f"Processing file: ________{file}_________\n\n")
 
         with open(file_path, 'r') as f:
             data = json.load(f)
 
         if "annotations" not in data:
-            print(f"Warning: 'annotations' key not found in file {file}.")
             continue
 
         if "images" not in data:
-            print(f"Warning: 'images' key not found in file {file}.")
-            continue
+            width = 224
+            height = 224
+        else:
+            width = data['images'][0]['width']
+            height = data['images'][0]['height']
 
         for annotation in data['annotations']:
-            #print(annotation)
             try:
                 keypoints = annotation['keypoints']
             except KeyError:
-                print(f"Warning: 'keypoints' key not found in an annotation in file {file}.")
-                print(f"Problematic annotation: {annotation}")
                 continue
 
             num_keypoints = len(keypoints) // 3
             if num_keypoints < 15:
-                print(f"Warning: Insufficient number of keypoints in an annotation in file {file}.")
-                print(f"Problematic annotation: {annotation}")
                 continue
 
-            width = data['images'][0]['width']
-            height = data['images'][0]['height']
             normalized_keypoints = normalize_keypoints(keypoints, width, height)
-            all_data.append(normalized_keypoints)
-            all_labels.append(label)
+            file_data.append(normalized_keypoints)
+
+        # Create sequences from the file_data if there are enough frames
+        if len(file_data) >= sequence_length:
+            file_data = np.array(file_data)
+            file_sequences = create_sequences(file_data, sequence_length, step_size)
+            file_labels = np.full(file_sequences.shape[0], label_to_index[label])
+
+            all_data.extend(file_sequences)
+            all_labels.extend(file_labels)
+
+        print(f"Appended label {label} for file {file}")
 
     all_data = np.array(all_data)
     all_labels = np.array(all_labels)
-
+    print(f"Data length for {label}: {len(all_data)}")
+    print(f"Labels length for {label}: {len(all_labels)}")
     return all_data, all_labels
 
 # Example usage
 
+folder_path = 'D:\\AIPROJECT2\\preprocessed_data\\json_pushup'
+folder_path2 = 'D:\\AIPROJECT2\\preprocessed_data\\json_curl'
+folder_path3 = 'D:\\AIPROJECT2\\preprocessed_data\\json_fly'
+folder_path4 = 'D:\\AIPROJECT2\\preprocessed_data\\json_OHP'
+folder_path5 = 'D:\\AIPROJECT2\\preprocessed_data\\json_squats'
+folder_path6 = 'D:\\AIPROJECT2\\preprocessed_data\\json_NonExcercise'
+output_folder = 'D:\\AIPROJECT2\\output'
 
-folder_path = 'D:\\AIPROJECT\\preprocessed_data\\json_pushup'
-folder_path2 = 'D:\\AIPROJECT\\preprocessed_data\\json_curl'
-folder_path3 = 'D:\\AIPROJECT\\preprocessed_data\\json_fly'
-folder_path4 = 'D:\\AIPROJECT\\preprocessed_data\\json_OHP'
-folder_path5 = 'D:\\AIPROJECT\\preprocessed_data\\json_squats'
-output_folder = 'D:\\AIPROJECT\\output'
-
-# max_z = find_max_z(folder_path)
-# print(max_z)
+NoExcercise, NoExcercise_labels = preprocess_data(folder_path6, "non_exercise")
+print("No Ecercise data preprocessing complated!")
 
 pushup_data, pushup_labels = preprocess_data(folder_path, "pushup")
 print("Pushup data preprocessing complated!")
@@ -146,8 +133,16 @@ print("SQUAT data preprocessing complated!")
 ohp_data, ohp_labels = preprocess_data(folder_path5, "ohp")
 print("OHP data preprocessing complated!")
 
-all_data = np.concatenate(( pushup_data,curl_data,fly_data,squat_data,  ohp_data), axis=0)
-all_labels = np.concatenate((pushup_labels,curl_labels,fly_labels ,squat_labels,ohp_labels), axis=0)
+print(pushup_data.shape)
+print(curl_data.shape)
+print(fly_data.shape)
+print(squat_data.shape)
+print(ohp_data.shape)
+print(NoExcercise.shape)
+
+
+all_data = np.concatenate(( pushup_data,curl_data,fly_data,squat_data,  ohp_data,NoExcercise), axis=0)
+all_labels = np.concatenate((pushup_labels,curl_labels,fly_labels ,squat_labels,ohp_labels,NoExcercise_labels), axis=0)
 
 
 # Reorder data and labels using the random indices
@@ -160,14 +155,10 @@ X_train, X_test, y_train, y_test = train_test_split(all_data, all_labels, test_s
 # Further split the training data into training and validation sets (80% training, 20% validation)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=42, stratify=y_train)
 
-# Save it all for later use
+
 np.save(os.path.join(output_folder, "X_train.npy"), X_train)
-np.save(os.path.join(output_folder, "y_train.npy"), y_train)
-np.save(os.path.join(output_folder, "X_val.npy"), X_val)
-np.save(os.path.join(output_folder, "y_val.npy"), y_val)
 np.save(os.path.join(output_folder, "X_test.npy"), X_test)
+np.save(os.path.join(output_folder, "X_val.npy"), X_val)
+np.save(os.path.join(output_folder, "y_train.npy"), y_train)
 np.save(os.path.join(output_folder, "y_test.npy"), y_test)
-
-
-#np.save(save_path, all_data)
-#np.save(save_path, all_labels)
+np.save(os.path.join(output_folder, "y_val.npy"), y_val)
